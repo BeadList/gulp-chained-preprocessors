e514292ce39ac25f720fbd0993e33f17873d8db9
Dmitry Yakimov <welldan97@gmail.com>
2016-03-19 18:20:35 +0300

    :truck:

--- dist/gulp-chained-preprocessors.js
+++ dist/gulp-chained-preprocessors.js
@@ -4,6 +4,10 @@ Object.defineProperty(exports, "__esModule", {
   value: true
 });
 
+var _path = require('path');
+
+var _path2 = _interopRequireDefault(_path);
+
 var _through = require('through2');
 
 var _through2 = _interopRequireDefault(_through);
@@ -14,26 +18,25 @@ var _chainedPreprocessors2 = _interopRequireDefault(_chainedPreprocessors);
 
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
-/*global Buffer, module */
-
-
 var gulpChainedPreprocessors = function gulpChainedPreprocessors(options) {
   return _through2.default.obj(function (file, enc, gulpCb) {
     var contents = file.contents.toString('utf8');
     var extensions = _chainedPreprocessors2.default.extensionsToPreprocess(file.path);
-    var path = _chainedPreprocessors2.default.preprocessedName(file.path);
-
+    var resultPath = _chainedPreprocessors2.default.preprocessedName(file.path);
+    options.sass = options.sass || {};
+    options.sass.includePaths = options.sass.includePaths || [_path2.default.dirname(file.path)];
     _chainedPreprocessors2.default.render(contents, extensions, options, function (err, result) {
       if (err) {
         gulpCb(err, null);
         return;
       }
       file.contents = new Buffer(result);
-      file.path = path;
+      file.path = resultPath;
       gulpCb(err, file);
     });
   });
-};
+}; /*global Buffer, module */
+
 
 module.exports = gulpChainedPreprocessors;
 exports.default = gulpChainedPreprocessors;
