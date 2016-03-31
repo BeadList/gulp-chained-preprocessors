c7e8623780b6cebc6ca970282caeb3cb3093c088
Dmitry Yakimov <welldan97@gmail.com>
2016-03-31 17:31:06 +0300

    :truck:

--- dist/gulp-chained-preprocessors.js
+++ dist/gulp-chained-preprocessors.js
@@ -18,11 +18,17 @@ var _chainedPreprocessors2 = _interopRequireDefault(_chainedPreprocessors);
 
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
-var gulpChainedPreprocessors = function gulpChainedPreprocessors(options) {
+var gulpChainedPreprocessors = function gulpChainedPreprocessors(inOptions) {
   return _through2.default.obj(function (file, enc, gulpCb) {
     var contents = file.contents.toString('utf8');
     var extensions = _chainedPreprocessors2.default.extensionsToPreprocess(file.path);
     var resultPath = _chainedPreprocessors2.default.preprocessedName(file.path);
+    var options = void 0;
+    if (typeof inOptions == 'function') {
+      options = inOptions(file);
+    } else {
+      options = Object.assign({}, inOptions);
+    }
     options.sass = options.sass || {};
     options.sass.includePaths = options.sass.includePaths || [_path2.default.dirname(file.path)];
     _chainedPreprocessors2.default.render(contents, extensions, options, function (err, result) {
